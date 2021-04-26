25e4e4284b35ba01ad452ccc9a9ccd47a1e39d55
MAMOUN-kamal-alshisani <mamoun.alshishani@yahoo.com>
2021-04-26 13:57:56 +0300

    my new about me

---
+++ app.js
@@ -0,0 +1,67 @@
+//lab about me
+
+
+let condi=prompt('are you good?');
+console.log(condi);
+
+switch (good) {
+    case 'yes':
+        console.log('ok have a nice day');
+break ;
+case 'no':
+console.log('i hope it gets better soon');
+
+}
+
+
+ let age=prompt('are you over 18?');
+   console.log(age);
+
+switch (age) {
+    case 'yes':
+
+        console.log('ok you can check any infromation you like');
+break ;
+case 'no' :
+
+        console.log('you cant access some information');
+
+}
+
+
+let live=prompt('do you live in jordan?');
+console.log(live);
+
+switch (live) {
+    case 'yes':
+
+        console.log('ok feel free');
+break ;
+case 'no' :
+
+        console.log('you need to register');
+
+
+}
+
+
+let work=prompt('do you work?');
+console.log(work);
+
+switch (work) {
+    case 'yes':
+
+        console.log('ok feel free');
+break ;
+case 'no' :
+
+        console.log('you need to register');
+
+
+}
+
+
+
+
+
+alert('hello ' + yourname +' your age is ' + age +' you live in ' + live +' your fav color is ' +color);
