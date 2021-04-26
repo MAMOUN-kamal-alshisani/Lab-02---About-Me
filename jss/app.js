38db616eda82c8f2c46090baf2525d9f4376d155
MAMOUN-kamal-alshisani <mamoun.alshishani@yahoo.com>
2021-04-26 16:08:25 +0300

    i completed lab about me

--- jss/app.js
+++ jss/app.js
@@ -1,49 +1,53 @@
 //lab about me
+'use strict';
 let yourname=prompt('what is your name?');
-alert('hello' +yourname + 'and welcome');
+console.log(yourname);
+alert('hello ' +yourname + ' and welcome');
 
 let condi=prompt('are you good?');
 console.log(condi.toUpperCase());
 
-switch (condi) {
+switch (condi.toLowerCase()) {
     case 'yes':
-        console.log('ok have a nice day');
+            if(condi ==='yes'){
+        console.log('ok have a nice day');}
+        
 break ;
 case 'no':
 console.log('i hope it gets better soon');
-
+alert('you answered with no');
 }
 
 
  let age=prompt('are you over 18?');
-   console.log(age.toUpperCase());
+   console.log(age);
 
-switch (age) {
+switch (age.toLowerCase()) {
     case 'yes':
-
+if(age==='yes');{
         console.log('ok you can check any infromation you like');
-        alert('your answer is yes');
+        }
 break ;
 case 'no' :
 
         console.log('you cant access some information');
-
+        alert('you answered with no');
 }
 
 
 let live=prompt('do you live in jordan?');
 console.log(live);
 
-switch (live.toUpperCase()) {
+switch (live.toLowerCase()) {
     case 'yes':
-
+if(age ==='yes');{
         console.log('ok feel free');
-        alert('your answer is yes');
+       }
 break ;
 case 'no' :
 
         console.log('you need to register');
-
+        alert('you answered with no');
 
 }
 
@@ -51,32 +55,32 @@ case 'no' :
 let work=prompt('do you work?');
 console.log(work);
 
-switch (work.toUpperCase()) {
+switch (work.toLowerCase()) {
     case 'yes':
-
+        if(work ==='yes');{
         console.log('ok feel free');
-        alert('your answer is yes');
+        }
 break ;
 case 'no' :
 
         console.log('you need to register');
-
+        alert('you answered with no');
 
 }
 
 let hardworker=prompt('do you workhard?');
-console.log(hardwork);
+console.log(hardworker);
 
-switch (work.toUpperCase()) {
+switch (hardworker.toLowerCase()) {
     case 'yes':
-
-        console.log('keep iy up!');
-        alert('your answer is yes');
+if (hardworker ==='yes'){
+        console.log('keep it up!');}
+        
 break ;
 case 'no' :
 
         console.log('you should do better');
-
+        alert('you answered with no');
 }
 
-alert('hello ' + yourname + 'i hope you found what you were looking for');
+alert('hello ' + yourname + ' i hope you found what you were looking for');
