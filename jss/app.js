0f1304767de9e2ed9ba5c4c6eb75856280450fee
MAMOUN-kamal-alshisani <mamoun.alshishani@yahoo.com>
2021-04-29 18:34:33 +0300

    about me extended

--- jss/app.js
+++ jss/app.js
@@ -1,74 +1,72 @@
 'use strict';
 let score = 0 ;
+
 function getusername  (){
 let yourname=prompt('what is your name?');
-console.log(yourname);
 alert('hello ' + yourname + ' and welcome');
 }
 getusername();
 
 function getquestions1 () {
-let my1name=prompt('is my name mamoun?');
-console.log(my1name.toUpperCase());
+let my1name=prompt('is my name mamoun alshishani?');
+
 switch (my1name.toLowerCase()) {
     case 'yes':
             case 'y':
                 score++;
                   alert('your answer is correct');
-      
-        console.log(my1name);
+    
 
 break ;
 case 'no':
         case 'n':
         alert('uncorrect');
-console.log('my name is mamoun');
 
+        alert('my name is mamoun alshishani');
 }
+
 }
 getquestions1()
 
 
 function getquestions2 () {
- let age2=prompt('am i 30 years old?');
-   console.log(age2.toLocaleLowerCase());
-
+ let age2=prompt('do you think i can drive a car');
+  
 switch (age2.toLocaleLowerCase()) {
     case 'yes':
         case 'y':
-                score++;
+                ;
         alert('your answer is uncorrect');
-        console.log(age2);
         
+
 break ;
 case 'no':
         case 'n':
+            score++
         alert('your answer is correct');
-        console.log('24 years old');
-        
+           
 }
+alert('no i cant, i can bairly even ride a bike');
 }
 getquestions2();
 
 
 function getquestions3 () {
 let li3ve=prompt('is this my email :mamoun.alshishani@yahoo.com ?');
-console.log(li3ve);
 
-switch (li3ve.toLowerCase()) {
+switch (li3ve.toLocaleLowerCase()) {
     case 'yes':
         case 'y':
                 score++;
                 alert('your answer is correct');
-        console.log(li3ve);
+alert()
        
 break ;
 case 'no' :
         case 'n':
-                alert('your answer is uncorrect');
-        console.log(li3ve);
-        
+                alert('your answer is uncorrect'); 
 
+                alert('my email is : mamoun.alshishani@yahoo.com');
 }
 }
 getquestions3();
@@ -79,46 +77,46 @@ let deve4lop=prompt('do you think im a programmer?');
 console.log(deve4lop);
 
 switch (deve4lop.toLowerCase()) {
-    case 'yes':
-        case 'y':
+    case 'no':
+        case 'no':
                 score++;
                 alert('your answer is correct');
        
         
 break ;
-case 'no' :
-        case 'n':
+case 'yes' :
+        case 'yes':
                 alert('your answer is uncorrect');
         
         
 
 }
+alert('not a programmer yet just a student for now');
 }
 getquestions4 ();
 
 function getquestions5 () {
 let hard5worker=prompt('do i have a bachelor\'s degree?');
-console.log(hard5worker);
 
-switch (hard5worker.toLowerCase()) {
+switch (hard5worker.toLocaleUpperCase()) {
     case 'yes':
         case 'y':
                 score++;
                 alert('your answer is correct');
-        console.log('yes');
         
 break ;
 case 'no' :
         case 'n':
                 alert('your answer is uncorrect');
-        console.log('no');
+        
         
 }
+
 }
 getquestions5()
 
 function getquestions6 () {
-                        alert('For this question you only have 4 attempts only')
+                        alert('For this question you only have 4 attempts only');
 
 
 
@@ -126,9 +124,7 @@ function getquestions6 () {
                         let my6age ;
                         for(let i = 1; i <= 4; i++){
                            my6age= Number(prompt('how old am i?'));
-                            while(!my6age){
-                                my6age = Number(prompt('Please Enter a Number Value'));
-                            }
+                           
                             if(my6age == 24){
                                 alert('Correct Answar good job');
                                 score++;
@@ -140,8 +136,6 @@ function getquestions6 () {
                             }
                         }
                         alert('The Correct answar is 24');
-                        
-                        
                         alert('For this question you only have 6 attempts only');
                 }
 getquestions6()
@@ -149,23 +143,25 @@ getquestions6()
 
 
 function getquestions7 () {                       
-let movies = ['castaway', 'monster huneter ','resident evil','saw'];
+let movies = ['castaway', 'monster hunter ','resident evil','saw','lucy'];
 let my7movie;
+
 for(let i = 1 ; i <=6 ;i++){
-    my7movie = prompt('Name one strategy game from my top 10 Video games');
+    my7movie = prompt('Name one movie from my top 10');
+    
+alert('castaway , monster huneter , resident evil , saw , lucy');
+    if(my7movie.toLowerCase() === movies [0] ||  my7movie === movies[1] || my7movie ===movies[2] || my7movie ===movies[3] || my7movie ===movies[4]){
 
-
-    if(my7movie.toLowerCase() === movies [0] ||  my7movie === movies[1] || my7movie ===movies[2] || my7movie ===movies[3]){
     alert('you are correct');
     score++;
     break;
 }else {
-    alert('wrong answar please try agian');
+    alert('wrong answar please try again');
+
+    alert('castaway', 'monster hunter ','resident evil','saw','lucy');
 }
 }
-alert(movies[0]+'/ '+ movies[1] +'/ '+ movies [2] +'/ '+ movies[3]);
-
-
 alert('you are good ' + 'your score is ' + score + ' out of 7');
 }
 getquestions7();
+
